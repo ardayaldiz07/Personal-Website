17664da71fa7296db7002e0fbe59531f2660c1a6
Arda Yaldız <99124364+ardayaldiz07@users.noreply.github.com>
2023-04-20 19:49:12 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        bro: ['./src/js/main.js','./src/sass/main.scss']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].min.js',
+    },
+    optimization: {
+		removeEmptyChunks: true,
+	},    
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new RemoveEmptyScriptsPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css',
+        }),
+    ]
+};
